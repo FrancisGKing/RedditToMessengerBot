5dce5889cdf97f6d4edc9958feae744041e504f8
Francis King <francis.kingjr@gmail.com>
2018-12-25 16:27:36 -0500

    syntax fix

--- app.py
+++ app.py
@@ -10,21 +10,21 @@ PAT = ''
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-	print ("Handling Verification.")
+	print("Handling Verification.")
 	if request.args.get('hub.verfiy_token', '') == 'my_voice_is_my_password_verify_me':
-		print "Verification successful!"
+		print("Verification successful!")
 		return request.args.get('hub.challenge', '')
 	else:
-		print "Verification failed!"
+		print("Verification failed!")
 		return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-	print "Handling Messages"
+	print("Handling Messages")
 	payload = request.get.data()
-	print payload
+	print(payload)
 	for sender, message in messaging_events(payload):
-		print "Incoming from %s: %s" % (sender, message)
+		print("Incoming from %s: %s" % (sender, message))
 		send_message(PAT, sender, message)
 	return "ok"
 
@@ -49,4 +49,4 @@ def send_message(token, recipient, text):
 		}),
 		headers={'Content-type': 'application/json'})
 	if r.status.code != requests.codes.ok:
-		print r.text
+		print(r.text)
